cb84b8775ac4ac0b4fdda2e2bc526f6f17c805c1
Jumail Mundekkat <mundekkat@hotmail.com>
2020-09-12 17:50:47 +1000

    Fix up imm.unsubscribe; Add comments

--- util/imm.js
+++ util/imm.js
@@ -1,8 +1,18 @@
+/*
+  Simple generic messaging bus
+  Could I just have use Node.JS emitters? ...Yes probably, but I didn't think of it till I was done
+  This has better logging for my sanity anyway
+*/
+
+// Construct with logger instance
 module.exports = (logger) => {
+  // Store of topics, with last data and subscribers
 	var topics = {};
 
 	return {
     
+    // Create a new topic
+    // Assumes topic doesn't presently exist
 		newTopic: (name) => {
 			if (name in topics) {
 				logger.error(`Topic ${topic} already exists`);
@@ -16,6 +26,8 @@ module.exports = (logger) => {
       logger.info(`New topic ${name}`, 3);
     },
     
+    // Delete an existing topic
+    // Assumes topic does exist
     deleteTopic: (name) => {
 			if (!(name in topics)) {
 				logger.error(`Topic ${topic} does not exist`);
@@ -26,6 +38,9 @@ module.exports = (logger) => {
       logger.info(`Deleted topic ${name}`, 3);
     },
     
+    // Add function as listener to topic
+    // Must be defined as a standard function, not an arrow function. Otherwise, func.name is null
+    // Assumes topic does exist
     subscribe: (topic, func) => {
       if (!(topic in topics)) {
 				logger.error(`Topic ${topic} does not exist`);
@@ -41,6 +56,8 @@ module.exports = (logger) => {
       logger.info(`Function ${func.name} subscribed to Topic ${topic}`, 3);
     },
 
+    // Remove function from topic listeners
+    // Assumes topic does exist
 		unsubscribe: (topic, func) => {
       if (!(topic in topics)) {
 				logger.error(`Topic ${topic} does not exist`);
@@ -48,8 +65,8 @@ module.exports = (logger) => {
       }
       
       for (var i = 0; i < topics[topic].subscribers.length; i++) {
-        if (topics[topic].subscribers[i]) {
-          topics[topic].subscribers.splice(i, 1);
+        if (topics[topic].subscribers[i].name == func.name) {
+          topics[topic].subscribers = topics[topic].subscribers.splice(i, 1);
           logger.info(`Function ${func.name} unsubscribed from Topic ${topic}`, 3);
           return;
         }
@@ -58,6 +75,8 @@ module.exports = (logger) => {
       logger.error(`Function ${func.name} was not subscribed to Topic ${topic}`);
 		},
 
+    // Call all subscribed functions for a topic with provided data asynchronously
+    // Assumes topic does exist
 		notify: (topic, data) => {
       if (!(topic in topics)) {
 				logger.error(`Topic ${topic} does not exist`);
@@ -71,6 +90,8 @@ module.exports = (logger) => {
       })
     },
     
+    // Get the last data that was added to the topic
+    // Assumes topic does exist
     getLastMessage: (topic) => {
       if (!(topic in topics)) {
 				logger.error(`Topic ${topic} does not exist`);
